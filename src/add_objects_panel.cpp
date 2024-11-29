870236895444685c6c030634e96e8d83d49ed625
LuCarpentier92 <lucas.carpentier.1@ens.etsmtl.ca>
2024-11-29 10:42:50 -0500

    continue logic for adding objects

--- src/add_objects_panel.cpp
+++ src/add_objects_panel.cpp
@@ -1,4 +1,5 @@
 #include <curobo_rviz/add_objects_panel.hpp>
+#include <string>
 
 namespace add_objects
 {
@@ -22,12 +23,46 @@ namespace add_objects
     void AddObjectsPanel::on_pushButtonAdd_clicked()
     {
         // retrieve values on the UI
-        double posX = doubleSpinBoxPositionX, posY = doubleSpinBoxPositionY, posZ = doubleSpinBoxPositionZ;
-        double orientationX = doubleSpinBoxxOrientationX, orientationY = doubleSpinBoxOrientationY, orientationZ = doubleSpinBoxOrientationZ, orientationW = doubleSpinBoxOrientationW;
-        double dimX = doubleSpinBoxDimensionsX, dimY = doubleSpinBoxDimensionsY, dimZ = doubleSpinBoxDimensionsZ;
-        double colorR = doubleSpinBoxColorA, colorG = doubleSpinBoxColorG, colorB = doubleSpinBoxColorB, colorA = doubleSpinBoxColorA;
+        double posX = ui_->doubleSpinBoxPositionX->value();
+        double posY = ui_->doubleSpinBoxPositionY->value();
+        double posZ = ui_->doubleSpinBoxPositionZ->value();
+        double orientationX = ui_->doubleSpinBoxxOrientationX->value();
+        double orientationY = ui_->doubleSpinBoxOrientationY->value();
+        double orientationZ = ui_->doubleSpinBoxOrientationZ->value();
+        double orientationW = ui_->doubleSpinBoxOrientationW->value();
+        double dimX = ui_->doubleSpinBoxDimensionsX->value();
+        double dimY = ui_->doubleSpinBoxDimensionsY->value();
+        double dimZ = ui_->doubleSpinBoxDimensionsZ->value();
+        double colorR = ui_->doubleSpinBoxColorA->value();
+        double colorG = ui_->doubleSpinBoxColorG->value();
+        double colorB = ui_->doubleSpinBoxColorB->value();
+        double colorA = ui_->doubleSpinBoxColorA->value();
+        int type;
+        string name;
+
 
         // retrieve type # from type string
+        QString textType = ui_->comboBoxObjects->currentData();
+        switch (textType) {
+            case 'Cube':
+                type = add_objects_client_.CUBOID;
+                break;
+            case 'Sphere':
+                type = add_objects_client_.SPHERE;
+                break;
+            case 'Capsule':
+                type = add_objects_client_.CAPSULE;
+                break;
+            case 'Cylindre':
+                type = add_objects_client_.CYLINDER;
+                break;
+            case 'Mesh':
+                type = add_objects_client_.MESH;
+                break;
+            default:
+                RCLCPP_WARNING(node_->get_logger(), "Couldn't find the type.");
+                break;
+        }
 
         // name the object
 
@@ -35,6 +70,8 @@ namespace add_objects
         // add_objects_request_ = std::make_shared<>();
 
         // call Display service to add the object on the screen
+
+        // add item to QListWidget (store the name to easily retrieve the name for suppression later on)
     }
 
     void AddObjectsPanel::on_pushButtonRemove_clicked()
